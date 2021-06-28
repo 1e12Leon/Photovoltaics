faba3ca2b59eb7e25a4224427defc9877118c683
Suki <1275506441@qq.com>
2021-06-28 15:24:15 +0800

    add project

---
+++ Photovoltaics/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Photovoltaics project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Photovoltaics.settings')
+
+application = get_asgi_application()
